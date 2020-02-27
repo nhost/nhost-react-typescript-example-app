cbe09a09b52b1ad75da1e61c7dd90ded378b8f52
Johan Eliasson <johan@eliasson.me>
2020-02-27 19:56:02 +0100

    ssl comment clarification

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+// make sure to get the ws/wss and http/https correct.
+
 exports.HASURA_GQE_ENDPOINT_WS = 'ws://localhost:8080/v1/graphql';
 exports.HASURA_GQE_ENDPOINT_HTTP = 'http://localhost:8080/v1/graphql';
 exports.BACKEND_ENDPOINT = 'http://localhost:4000';
