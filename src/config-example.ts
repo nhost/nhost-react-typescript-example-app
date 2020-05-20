6f9f10645ef95b2d93d3d1c65aaab6f9b8f2a314
elitan <johan@eliasson.me>
2020-05-20 10:04:49 +0200

    Using example config files

---
+++ src/config-example.ts
@@ -0,0 +1,2 @@
+export const BACKEND_ENDPOINT = "https://backend-[id].nhost.app";
+export const GRAPHQL_ENDPOINT = "https://hasura-[id].nhost.app/v1/graphql";
--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const BACKEND_ENDPOINT = "http://localhost:3000";
-export const GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql";
